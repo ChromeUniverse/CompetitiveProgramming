4d5ba7e0122544d76eb453fa7348cee51e43cd29
ChromeUniverse <33258966+ChromeUniverse@users.noreply.github.com>
2018-12-16 12:29:59 -0300

    Create fuga.cpp

---
+++ OBI/fuga.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    int h, p ,f ,d;
+    cin >> h >> p >> f >> d;
+
+    int position = f;
+    while (true) {
+        position += d;
+        if (position < 0){
+          position += 16;
+        }
+        if (position > 15){
+          position -= 16;
+        }
+        if (position == p) {
+            cout << 'N';
+            break;
+        }
+        if (position == h) {
+            cout << 'S';
+            break;
+            }
+  }
+
+}
